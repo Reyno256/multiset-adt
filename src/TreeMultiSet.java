85801f269e7679863b6a1416a83ddd1a1e33df34
ceryanki <yanki.cer@mail.utoronto.ca>
2024-09-16 14:41:44 -0400

    Updated the TreeMultiSet.java according to Multiset.java

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -9,7 +9,29 @@ public class TreeMultiSet extends MultiSet {
     @Override
     void add(Integer item) {
         // TODO complete this method
+        throw new UnsupportedOperationException("Not supported");
     }
+    void remove(Integer item) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    boolean contains(Integer item) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    int count(Integer item) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    int size(Integer item) {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+
     // TODO write the rest of the implementation for this class so that it uses its private Tree
     //      object to provide the MultiSet functionality.
 }
